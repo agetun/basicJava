085fb535facafadff6a33f7fc80724322ddf43b2
Aleksandr <sashagetun@gmail.com>
2022-12-13 20:40:07 +0100

    Test

---
+++ src/lesson4/DogDemo.java
@@ -0,0 +1,24 @@
+package lesson4;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+
+        Dog myDog = new Dog();
+
+        myDog.name = "Lessy";
+        myDog.breed = "Kolli";
+        myDog.age = 3;
+
+        myDog.voice("Gav! Gav! Gav!");
+
+        myDog.age = myDog.age+1;
+
+        System.out.println(myDog);
+
+        Dog myDog2 = new Dog("Tuzik", 5);
+
+
+
+    }
+}
