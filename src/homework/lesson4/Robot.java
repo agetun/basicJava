0cf9204ce60e0e9eaef79db9f9f604346bd72321
Aleksandr <sashagetun@gmail.com>
2022-12-14 21:58:30 +0100

    Test1

--- src/homework/lesson4/Robot.java
+++ src/homework/lesson4/Robot.java
@@ -1,4 +1,11 @@
 package homework.lesson4;
 
 public class Robot {
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    public Robot() {
+    }
 }
---
+++ src/homework/lesson4/RobotDemo.java
@@ -0,0 +1,13 @@
+package homework.lesson4;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot myRobot = new Robot();
+
+
+    }
+
+
+}
