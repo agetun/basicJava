cba35095a1b06e538af5be14ef1b7c2cb39edcb9
Aleksandr <sashagetun@gmail.com>
2022-12-18 16:59:22 +0100

    Test1

--- src/homework/lesson5/Task2.java
+++ src/homework/lesson5/Task2.java
@@ -1,4 +1,35 @@
 package homework.lesson5;
 
+import javax.swing.text.StringContent;
+
 public class Task2 {
+
+    public static void main(String[] args) {
+
+        String text = "I study Basic Java!";
+
+        StringMethodsTest(text);
+
+    }
+
+    public static void StringMethodsTest(String text){
+
+        String str1 = new String(text);
+        System.out.println(text);
+
+        char simbol = text.charAt(18);
+
+        System.out.println(simbol);
+
+        System.out.println(text.contains("Java"));
+
+
+//Остальное не успеваю разобраться как осуществить замену символов,
+// преобразовать в верхний и нижний регистры, вырезать строку.
+
+
+
+
+    }
+
 }
