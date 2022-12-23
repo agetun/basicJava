dc8867b12ab3e73be4cce897ce13519e2152c6ca
Aleksandr <sashagetun@gmail.com>
2022-12-23 02:14:19 +0100

    Test1

---
+++ src/lessons/lesson9/CompareNumberDemo.java
@@ -0,0 +1,35 @@
+package lessons.lesson9;
+
+import java.util.Scanner;
+
+public class CompareNumberDemo {
+
+    /*
+Написать программу, которая запрашивает у пользователя
+два целых числа и выводит на консоль наименьшее из них.
+   */
+    public static void main(String[] args) {
+
+        CompareNumbers compareNumbers= new CompareNumbers();
+
+        compareNumbers.number1= userInput("Please, enter first number");
+        compareNumbers.number2 = userInput("Please, enter second number");
+
+        compareNumbers.printInfo(" Lesser number ", compareNumbers.compareLesser(), true);
+        compareNumbers.printInfo(" Maximum number is ", compareNumbers.compareMaximum(),true);
+
+
+        if (compareNumbers.isEqual()){compareNumbers.printInfo("Numbers are equals",0,false);}
+        else {compareNumbers.printInfo("Numbers are different",0, false);}
+
+    }
+
+
+    public static double userInput(String message){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        return scanner.nextDouble();
+
+    }
+
+}
---
+++ src/lessons/lesson9/CompareNumbers.java
@@ -0,0 +1,36 @@
+package lessons.lesson9;
+
+public class CompareNumbers {
+
+    double number1;
+
+    double number2;
+
+
+    public double compareLesser(){
+        if (number1<number2) {return  number1;}
+        else {return number2;}
+
+    }
+
+    public double compareMaximum(){
+        if (number1>number2) {return  number1;}
+        else {return number2;}
+
+    }
+
+    public boolean isEqual(){
+        if (number1==number2) {return true;}
+        else {return false;}
+    }
+
+    public void printInfo(String message, double compareResult, boolean check){
+        System.out.println("--------");
+        String outputMessage;
+        if (check) {outputMessage = message  + compareResult;}
+        else { outputMessage = message;}
+
+        System.out.println(outputMessage);
+    }
+
+}
