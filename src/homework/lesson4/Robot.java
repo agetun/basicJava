c7f8cc52e149d08b9bc79fef47e476ab7e67ef32
Aleksandr <sashagetun@gmail.com>
2022-12-26 02:01:18 +0100

    Test1

--- src/homework/lesson4/Robot.java
+++ src/homework/lesson4/Robot.java
@@ -2,11 +2,17 @@ package homework.lesson4;
 
 public class Robot {
 
+    String name;
     public void sayHello() {
 
         System.out.println("Hello");
     }
 
-    public Robot() {
+    public Robot(String name) {
+        this.name = name;
+    }
+
+    public void sayYourName(){
+        System.out.println("My name is " + this.name);
     }
 }
--- src/homework/lesson4/RobotDemo.java
+++ src/homework/lesson4/RobotDemo.java
@@ -4,9 +4,13 @@ public class RobotDemo {
 
     public static void main(String[] args) {
 
-        Robot robot1 = new Robot();
+        Robot robot1 = new Robot("Rider"); // этого робота зовут Rider
+        robot1.sayHello();
+        robot1.sayYourName();
 
-
+        Robot robot2 = new Robot("John"); // этого John
+        robot2.sayHello();
+        robot2.sayYourName();
 
 
     }
