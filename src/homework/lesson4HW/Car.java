fbc27d1b4d8a18db67c24a45bce2be66cc9240e9
Aleksandr <sashagetun@gmail.com>
2022-12-30 11:05:33 +0100

    Homework

--- src/homework/lesson4HW/Car.java
+++ src/homework/lesson4HW/Car.java
@@ -1,5 +1,33 @@
 package homework.lesson4HW;
+/*
+Найти ошибку в коде.
+Исправить и запустить программу.
+Менять можно только класс Car.
 
+class Car {
+
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel {
+        return this.model
+    }
+
+}
+
+class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+
+}
+ */
 public class Car {
 
     String model;
