04934fbe0f73f06dca652b50509ed792185e94ef
Aleksandr <sashagetun@gmail.com>
2023-01-04 20:22:44 +0100

    Homework

--- src/homework/lesson10SkillDesk/Task1_2.java
+++ src/homework/lesson10SkillDesk/Task1_2.java
@@ -4,7 +4,22 @@ public class Task1_2 {
 
     public static void main(String[] args) {
 
-        boolean isEdekaOpen = true;
-        boolean isReweOpen = true;
+        boolean isEdekaOpen=true;
+        boolean isReweOpen=false;
+
+        canBuy(isEdekaOpen,isReweOpen);
+
     }
-}
+
+    public static void canBuy(boolean Edeka, boolean Rewe){
+        if (Edeka == true || Rewe == true){
+            System.out.println("true");
+        }
+        else if (Edeka == false || Rewe == false) {
+            System.out.println("false");
+        }
+
+
+        }
+    }
+
