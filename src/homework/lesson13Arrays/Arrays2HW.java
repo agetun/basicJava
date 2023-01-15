ee7dea2b12320c96bc4297023ca824f255c2476f
Aleksandr <sashagetun@gmail.com>
2023-01-15 20:00:28 +0100

    Homework

--- src/homework/lesson13Arrays/Arrays2HW.java
+++ src/homework/lesson13Arrays/Arrays2HW.java
@@ -7,8 +7,21 @@ public class Arrays2HW {
         int[] arrays1 = new int[5];
 
         for (int i = 0; i < arrays1.length; i++) {
-            arrays1[i] = (int) (Math.random() * 98) + 10;
+            arrays1[i] = (int) (Math.random() * 90) + 10;
             System.out.print(arrays1[i] + "  ");
+        }
+
+        boolean flag = true;
+        for (int i = 1; i < arrays1.length; i++) {
+            if (arrays1[i] <= arrays1[i-1]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            System.out.println("\nArray up numbers");
+        } else {
+            System.out.println("\nArray not up numbers");
         }
 }
 
